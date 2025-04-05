b162267bb8befa9ebdc4a5b75de353ee922dfb5d
ts0826 <ts816456@naver.com>
2025-04-05 16:59:39 +0900

    Item
    4강. 무기와 아이템 프리팹 제작

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { Ammo,Coin,Grenade,Heart,Weapon};
+    public Type type;
+    public int Value;
+
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+    }
+
+}
